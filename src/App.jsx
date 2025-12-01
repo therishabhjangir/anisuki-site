4c3c42bce16c90d8285d7e655ec4debeb1497eff
therishabhjangir <rishabhjangirz@gmail.com>
2025-12-01 12:58:41 +0530

    Add Subjects component to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Gallery from "./components/Gallery"
 import Blog from "./components/Blog"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import Subjects from "./components/Subjects"
+
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
       <Header />
       <main className="py-6">
         <About />
+        <Subjects />
         <Gallery />
         <Blog />
         <Contact />
